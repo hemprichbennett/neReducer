3e8d42a000a2b7901b43903ba632ccf3830512b9
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-15 13:40:39 +0100

    Made basic package helpfile

---
+++ R/netReducer.R
@@ -0,0 +1,6 @@
+#' @details
+#' netReducer currently consists of only one basic command: \code{\link{netreducing}} for comparing how metrics vary
+#' depending on how many nodes are used and their identity, and \code{\link{shrink}}, which is used by  \code{\link{netreducing}}.
+#' No example data is yet included, see the readme at https://github.com/hemprichbennett/netReducer for examples.
+#' @keywords internal
+"_PACKAGE"
