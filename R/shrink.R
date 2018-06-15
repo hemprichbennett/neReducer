76073b27f00ac98a48ec0b533eb3111d8c7554f5
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-15 14:29:35 +0100

    Added option for if it is/isn't an index requiring a network level

--- R/shrink.R
+++ R/shrink.R
@@ -59,7 +59,7 @@ shrink <- function(mat, n_cols, itval=NA, netname = NA, metric, metric_type='net
 
   #Calculate the metric
   if(metric_type=='network'){
-    metricval <- bipartite::networklevel(shrunkmat, index = metric)
+    metricval <- bipartite::networklevel(shrunkmat, index = metric, level = network_level)
 
     #Add it to the metadata
     shrunkmeta$metricval <- rep(metricval, nrow(shrunkmeta))
