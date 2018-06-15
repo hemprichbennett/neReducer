58898770a83c97cefe5a572256e22d41da20ac61
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-15 11:20:56 +0100

    Messing around testing code and working out what to pass to the shrink function to work properly

--- R/earlytrial.R
+++ R/earlytrial.R
@@ -1,27 +1,50 @@
+library(DataExplorer)
+library(reshape2)
+trialmat1 <- matrix(sample(c(0,1),100, replace = T), nrow = 10)
+colnames(trialmat1) <- c('Hice-001', 'Hice-002',	'Rhtr-001',	'Rhtr-008',	 'Keha-0005', 'Keha-01A', 'Hice-005', 'Rhtr-010', 'Hidi-001', 'Hice-087')
+trialmat1
+trialmat2 <- matrix(sample(c(0,1),100, replace = T), nrow = 10)
+colnames(trialmat2) <- c('Hidy-001', 'Hidy-002',	'Rhtr-001',	'Rhtr-008',	 'Keha-0005', 'Keha-01A', 'Hidy-005', 'Rhtr-010', 'Hidi-001', 'Hidy-087')
 
-trialmat <- matrix(sample(c(0,1),100, replace = T), nrow = 10)
-colnames(trialmat) <- c('Hice-001', 'Hice-002',	'Rhtr-001',	'Rhtr-008',	 'Keha-0005', 'Keha-01A', 'Hice-005', 'Rhtr-010', 'Hidi-001', 'Hice-087')
+input_list <- list('trialmat1' = trialmat1, 'trialmat2' = trialmat2)
 
-shrink <- function(mat, n_cols){
+#write.csv(trialmat1, '~/Desktop/example_matrix.csv')
 
-  #Decide which rows to keep and which to discard
-  to_keep <- sample(seq(1, ncol(mat)), n_cols)
-  print(to_keep)
-  #Make a dataframe of all of their metadata
-  batmetadata <- data.frame(strsplit(colnames(mat), split = '-'))
-  batmetadata <- t(batmetadata)
-  batmetadata <- as.data.frame(batmetadata)
-  colnames(batmetadata) <- c('Species', 'ID')
-  #print(batmetadata)
-  batmetadata$orig <- colnames(mat)
-  batmetadata$orig <- as.factor(batmetadata$orig)
+source('R/shrink.R')
 
-  batmetadata$included <- rep(NA, nrow(batmetadata))
-  batmetadata$included[to_keep] <- 'included'
-  batmetadata$included[-to_keep] <- 'not_included'
-  batmetadata$included <- as.factor(batmetadata$included)
-  return(batmetadata)
+
+
+
+#A little function to pass values to shrink in a slightly easier manner
+setup <- function(iteration, net, netname=NA, n_col){
+  #cat('iteration is', iteration, '\n')
+  #cat('n_col is', n_col, '\n')
+  #iteration <- 1
+  #cat('netname is', netname, '\n')
+
+  return(shrink(net, n_cols = n_col, itval = iteration, netname = netname, metric = 'connectance'))
 }
 
-a <- shrink(mat = trialmat, n_cols = 4)
 
+#A horrendous giant nested apply, but it works
+n_iterations <- 100
+list_output <-  sapply(seq(1,n_iterations), function(it)
+  sapply(seq_along(input_list), function(y)
+    lapply(seq(5,ncol(input_list[[y]])-1), function(n) setup(iteration = it, net = input_list[[y]], netname = names(input_list)[y], n_col = n))) )
+
+
+bigdf <- do.call(rbind, list_output)
+
+
+
+
+
+
+
+mat_output <- sapply(seq(1,n_iterations), function(it)
+    sapply(seq(5,ncol(trialmat1)), function(n) setup(iteration = it, net = trialmat1, n_col = n)))
+
+
+tem <- lapply(seq(1, n_iterations), function(it) lapply(seq(5,ncol(trialmat1)-1), function(n) setup(iteration = it, net = trialmat1, n_col = n)))
+
+outdf <- do.call(rbind, lapply(tem, function(x) do.call(rbind, x)))
