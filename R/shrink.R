102d61d052c57e57baf8aac2918f352abe3cc42e
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-18 12:03:29 +0100

    Modified shrink so that it can work without splitting matrix column names

--- R/shrink.R
+++ R/shrink.R
@@ -20,44 +20,72 @@ shrink <- function(mat, n_cols, itval=NA, netname = NA, metric, metric_type='net
   #cat('n_cols to use is ', n_cols, '\n')
   #Make a dataframe of all of their metadata
   #print(netname)
-  shrunkmeta <- data.frame(strsplit(colnames(mat), split = '-'))
-  shrunkmeta <- t(shrunkmeta)
-  shrunkmeta <- as.data.frame(shrunkmeta)
-  colnames(shrunkmeta) <- c('Species', 'ID')
 
-  #Make a column of all the original column names
-  shrunkmeta$orig <- colnames(mat)
-  shrunkmeta$orig <- as.factor(shrunkmeta$orig)
+  if(collapse=T){
 
-  #Make a column showing which were included in the iteration
-  shrunkmeta$included <- rep(NA, nrow(shrunkmeta))
-  shrunkmeta$included[to_keep] <- 'included'
-  shrunkmeta$included[-to_keep] <- 'not_included'
-  shrunkmeta$included <- as.factor(shrunkmeta$included)
+    shrunkmeta <- data.frame(strsplit(colnames(mat), split = '-'))
+    shrunkmeta <- t(shrunkmeta)
+    shrunkmeta <- as.data.frame(shrunkmeta)
+    colnames(shrunkmeta) <- c('Species', 'ID')
 
-  #Optional formatting
-  if(!is.na(itval)){
-    shrunkmeta$iteration <- rep(itval, nrow(shrunkmeta))
-    shrunkmeta$n_used <- rep(n_cols, nrow(shrunkmeta))
+    #Make a column of all the original column names
+    shrunkmeta$orig <- colnames(mat)
+    shrunkmeta$orig <- as.factor(shrunkmeta$orig)
 
-  }
-  if(!is.na(netname)){
-    shrunkmeta$netnames <- rep(netname, nrow(shrunkmeta))
-  }
+    #Make a column showing which were included in the iteration
+    shrunkmeta$included <- rep(NA, nrow(shrunkmeta))
+    shrunkmeta$included[to_keep] <- 'included'
+    shrunkmeta$included[-to_keep] <- 'not_included'
+    shrunkmeta$included <- as.factor(shrunkmeta$included)
 
-  rownames(shrunkmeta) <- NULL
+    #Optional formatting
+    if(!is.na(itval)){
+      shrunkmeta$iteration <- rep(itval, nrow(shrunkmeta))
+      shrunkmeta$n_used <- rep(n_cols, nrow(shrunkmeta))
 
-  #Shrink the matrix to the desired columns
+    }
+    if(!is.na(netname)){
+      shrunkmeta$netnames <- rep(netname, nrow(shrunkmeta))
+    }
 
-  shrunkmat <- mat[,to_keep]
-  #print(cat('dim after', dim(shrunkmat), '\n'))
-  #Rename the columns so that everything after the hyphen is removed
-  colnames(shrunkmat) <- gsub('-.+', '', colnames(shrunkmat))
-  #merge the duplicate columns (drop=F is required so that it can still rowsum columns which are unique)
-  if(collapse==T){
+    rownames(shrunkmeta) <- NULL
+
+    #Shrink the matrix to the desired columns
+
+    shrunkmat <- mat[,to_keep]
+    #print(cat('dim after', dim(shrunkmat), '\n'))
+    #Rename the columns so that everything after the hyphen is removed
+    colnames(shrunkmat) <- gsub('-.+', '', colnames(shrunkmat))
+    #merge the duplicate columns (drop=F is required so that it can still rowsum columns which are unique)
+
     shrunkmat <- sapply(unique(colnames(shrunkmat)), function(x) rowSums(shrunkmat[,grepl(x, colnames(shrunkmat)), drop = F]))
-  }else{
-    shrunkmat <- mat[,to_keep] #For if we're not collapsing the columns (I.e. an individual-based analysis)
+
+  }
+  else if(collapse=F){
+    shrunkmeta$orig <- colnames(mat)
+    shrunkmeta$orig <- as.factor(shrunkmeta$orig)
+
+    #Make a column showing which were included in the iteration
+    shrunkmeta$included <- rep(NA, nrow(shrunkmeta))
+    shrunkmeta$included[to_keep] <- 'included'
+    shrunkmeta$included[-to_keep] <- 'not_included'
+    shrunkmeta$included <- as.factor(shrunkmeta$included)
+
+    #Optional formatting
+    if(!is.na(itval)){
+      shrunkmeta$iteration <- rep(itval, nrow(shrunkmeta))
+      shrunkmeta$n_used <- rep(n_cols, nrow(shrunkmeta))
+
+    }
+    if(!is.na(netname)){
+      shrunkmeta$netnames <- rep(netname, nrow(shrunkmeta))
+    }
+
+    rownames(shrunkmeta) <- NULL
+
+    #Shrink the matrix to the desired columns
+
+    shrunkmat <- mat[,to_keep]
   }
 
 
